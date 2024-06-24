deb58e321849a89526f583c2fc08541330d986ee
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-24 19:10:41 +0200

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,13 @@
-import os
 import pytest
 from app.main import get_coin_combination
 
 
-def test_get_coin_combination():
+def test_get_coin_combination() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
     assert get_coin_combination(6) == [1, 1, 0, 0]
     assert get_coin_combination(5) == [0, 1, 0, 0]
     assert get_coin_combination(10) == [0, 0, 1, 0]
-    assert  get_coin_combination(25) == [0, 0, 0, 1]
+    assert get_coin_combination(25) == [0, 0, 0, 1]
     assert get_coin_combination(50) == [0, 0, 0, 2]
     assert get_coin_combination(23) == [3, 0, 2, 0]
     assert get_coin_combination(18) == [3, 1, 1, 0]
